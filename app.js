4256c000a7bdc8373630c1d6aae4486e71471bfc
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2024-06-22 19:58:44 -0400

    Define a and b; get x; create base case for fibs

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 // fibonacci using iteration
-function fibs() {
+let a = 0;
+let b = 1;
+function fibs(x) {
+    if(x < 2) {
+        return 1
+    } else {
+        
+    }
     // take in a number x
     // run the fibonacci x times
     // return the result of the fib sequence after x iterations
