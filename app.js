c156aac0ff74a768b829a27841883d11c605362a
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2024-06-22 19:50:10 -0400

    Write psuedocode for fibs()

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 // fibonacci using iteration
 function fibs() {
-    
+    // take in a number x
+    // run the fibonacci x times
+    // return the result of the fib sequence after x iterations
 }
