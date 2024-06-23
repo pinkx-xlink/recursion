0296cc048c7b85b757262301a1fa1bb3b675cbec
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2024-06-22 20:20:56 -0400

    Set x to 8 and log it to console

--- app.js
+++ app.js
@@ -9,11 +9,13 @@ function fibs(x) {
     // run the fibonacci x times
     // return the result of the fib sequence after x iterations
 }
-console.log(fibs(8));
+x = 8;
+console.log(x);
+// console.log(fibs(8));
 
-function recursiveFib() {
-    if(x < 2) {
-        return 1
-    } else {
-    }
-}
+// function recursiveFib() {
+//     if(x < 2) {
+//         return 1
+//     } else {
+//     }
+// }
