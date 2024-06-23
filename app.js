024b194f89eea509b003c0784185935095f80289
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2024-06-22 22:00:31 -0400

    ESLint all indent problems

--- app.js
+++ app.js
@@ -3,21 +3,21 @@
 // fibonacci using iteration
 // take in a number x
 function fibs(d) {
-    let a = 0;
-    let b = 1;
-    let c;
-    let i = 2;
-    // run the fibonacci x times
-    while (i < d) {
-        c = a + b;
-        a = b;
-        b = c;
-        i++;
-        console.log(b)
-    }
-    console.log(`Rounds of fibonacci sequence executed: ${d} times`);
-    return b;
-    // return the result of the fib sequence after x iterations
+  let a = 0;
+  let b = 1;
+  let c;
+  let i = 2;
+  // run the fibonacci x times
+  while (i < d) {
+    c = a + b;
+    a = b;
+    b = c;
+    i++;
+    console.log(b)
+  }
+  console.log(`Rounds of fibonacci sequence executed: ${d} times`);
+  return b;
+  // return the result of the fib sequence after x iterations
 }
 fibs(8);
 
@@ -27,18 +27,18 @@ let y = 1;
 let z;
 let j = 0;
 function recursiveFib(num) {
-    // let j = 2;
-    if(num < 2) {
-        return 1;
-    } else {
-            z = x + y;
-            x++;
-            y++;
-            j++;
-            recursiveFib((num - 1));
-            console.log(z);
-    }
-    console.log(`Resursive fibonacci sequence executed: ${num} times.`);
-    return;
+  // let j = 2;
+  if(num < 2) {
+    return 1;
+  } else {
+    z = x + y;
+    x++;
+    y++;
+    j++;
+    recursiveFib((num - 1));
+    console.log(z);
+  }
+  console.log(`Resursive fibonacci sequence executed: ${num} times.`);
+  return;
 }
 recursiveFib(9);
