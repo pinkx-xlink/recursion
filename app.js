d9a9cf0ff0d94a1a4807af9dbe7e4b292e7a210b
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2024-06-22 20:05:07 -0400

    Add an else case to fibs()

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 // fibonacci using iteration
 let a = 0;
 let b = 1;
+// take in a number x
 function fibs(x) {
     if(x < 2) {
         return 1
     } else {
-        
+        fibs(x) = fib(x-1) = fibs(x-2)
     }
-    // take in a number x
     // run the fibonacci x times
     // return the result of the fib sequence after x iterations
 }
