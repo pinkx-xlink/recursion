61598871a91d33891a22c84fb934956dd34ae8b5
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2024-06-22 20:19:39 -0400

    Move base case from fibs to recursiveFibs()

--- app.js
+++ app.js
@@ -1,16 +1,19 @@
-// #!/usr/bin/env node
+#!/usr/bin/env node
 
 // fibonacci using iteration
 let a = 0;
 let b = 1;
 // take in a number x
 function fibs(x) {
-    if(x < 2) {
-        return 1
-    } else {
-        fibs(x) = fib(x-1) = fibs(x-2)
-    }
+   
     // run the fibonacci x times
     // return the result of the fib sequence after x iterations
 }
-fibs(8);
+console.log(fibs(8));
+
+function recursiveFib() {
+    if(x < 2) {
+        return 1
+    } else {
+    }
+}
