b3bd126472e615970d023f621a66b8d7d327cf1b
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2024-06-22 21:20:53 -0400

    Add number variables and an else case to recursiveFib()

--- app.js
+++ app.js
@@ -2,29 +2,38 @@
 // RUN THIS CODE IN NODE.JS
 // fibonacci using iteration
 // take in a number x
-function fibs(x) {
+function fibs(d) {
     let a = 0;
     let b = 1;
     let c;
     let i = 2;
     // run the fibonacci x times
-    while (i < x) {
+    while (i < d) {
         c = a + b;
         a = b;
         b = c;
         i++;
         console.log(b)
     }
-    console.log(`Rounds of fibonacci sequence executed: ${x} times`);
+    console.log(`Rounds of fibonacci sequence executed: ${d} times`);
     return b;
     // return the result of the fib sequence after x iterations
 }
 fibs(8);
 
 function recursiveFib(num) {
+    let x = 0;
+    let y = 1;
+    let z;
+    let j = 2;
     if(num < 2) {
         return 1
     } else {
+        z = x + y;
+        x = y;
+        y = z;
+        j++;
+        console.log(y);
         console.log(`Resursive fibonacci sequence executed: ${num} times.`);
     }
 }
